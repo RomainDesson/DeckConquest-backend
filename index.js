7e1aa19b9d2525a90bf806205c41573f54e5382e
romaindesson <romain.desson.02320@gmail.com>
2024-09-24 10:16:45 +0200

    Modify url

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 const PORT = process.env.PORT || 3000;  
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://nft-card-game-flame.vercel.app/",
     methods: ["GET", "POST"]
   }
 });
